235fae83803b6c8845b8426bec7560de6533abda
Jurek Sander <44340639+sanderjk5@users.noreply.github.com>
2021-09-17 08:29:33 +0200

    refactor: Removed logs.

--- src/server/controller/raptorAlgorithmController.ts
+++ src/server/controller/raptorAlgorithmController.ts
@@ -2,7 +2,6 @@ import { Converter } from "../../data/converter";
 import { GoogleTransitData } from "../../data/google-transit-data";
 import { RouteStopMapping } from "../../models/RouteStopMapping";
 import { StopTime } from "../../models/StopTime";
-import { performance } from 'perf_hooks';
 import { Sorter } from "../../data/sorter";
 import { JourneyResponse } from "../../models/JourneyResponse";
 import { Section } from "../../models/Section";
@@ -28,20 +27,12 @@ interface JourneyPointer {
     footpath: number,
 }
 
-
 export class RaptorAlgorithmController {
     private static earliestArrivalTimePerRound: number[][];
     private static earliestArrivalTime: number[];
     private static markedStops: number[];
     private static Q: QEntry[];
     private static j: JourneyPointer[];
-
-    private static timePart0;
-    private static timePart1;
-    private static timePart2;
-    private static timePart3;
-    private static timePart4;
-    private static timePart5;
 
     public static raptorAlgorithm(sourceStop: string, targetStop: string, sourceTime: string){
         console.time('raptor algorithm')
@@ -50,56 +41,31 @@ export class RaptorAlgorithmController {
         const targetStops = GoogleTransitData.getStopIdsByName(targetStop);
         this.init(sourceStops, sourceTimeInSeconds);
         this.performAlgorithm(targetStops);
-        console.log('Results:')
-        // for(let i = 0; i < targetStops.length; i++){
-        //     console.log(Converter.secondsToTime(this.earliestArrivalTime[targetStops[i]]));
-        // }
-        this.getJourneyResponse(sourceStops, targetStops);
-        
+        const journeyResponse = this.getJourneyResponse(sourceStops, targetStops);
         console.timeEnd('raptor algorithm')
-        
     }
 
     private static performAlgorithm(targetStops: number[]){
         let k = 0;
         while(true){
-            this.timePart0 = 0;
-            this.timePart1 = 0;
-            this.timePart2 = 0;
-            this.timePart3 = 0;
-            this.timePart4 = 0;
-            this.timePart5 = 0;
             k++;
             this.addNextArrivalTimeRound();
-            // console.log('Round ' + k);
-            // console.log(Converter.secondsToTime(this.earliestArrivalTime[targetStops[0]]));
             
             this.Q = [];
             let qTemp: QEntry[] = [];
             let routeSequenceMinima = new Array(GoogleTransitData.ROUTES.length);
             while(this.markedStops.length > 0){
-                // let part0Start = performance.now();
                 let markedStop = this.markedStops.pop();
-                // this.timePart0 += (performance.now() - part0Start);
-                // let part1Start = performance.now();
                 let routesServingStop: RouteStopMapping[] = GoogleTransitData.ROUTESSERVINGSTOPS[markedStop];
-                // this.timePart1 += (performance.now() - part1Start);
-                // let part2Start = performance.now();
                 for(let i = 0; i < routesServingStop.length; i++) {
                     let routeId = routesServingStop[i].routeId;
                     let stopSequence = routesServingStop[i].stopSequence;
-                    // let part3Start = performance.now();
                     if(!routeSequenceMinima[routeId] || stopSequence < routeSequenceMinima[routeId]){
                         routeSequenceMinima[routeId] = stopSequence;
                     }
-                    // this.timePart3 += (performance.now() - part3Start);
-                    // let part4Start = performance.now();
                     qTemp.push({r: routeId, p: markedStop, stopSequence: stopSequence});
-                    // this.timePart4 += (performance.now() - part4Start);
                 }
-                // this.timePart2 += (performance.now() - part2Start);
             }
-            // let part5Start = performance.now();
             for(let i = 0; i < qTemp.length; i++){
                 let qEntry = qTemp[i];
                 if(routeSequenceMinima[qEntry.r] === qEntry.stopSequence){
@@ -107,7 +73,6 @@ export class RaptorAlgorithmController {
                     routeSequenceMinima[qEntry.r] = - 1;
                 }
             }
-            // this.timePart5 += (performance.now() - part5Start);
 
             for(let i= 0; i < this.Q.length; i++){
                 let r = this.Q[i].r;
@@ -144,7 +109,6 @@ export class RaptorAlgorithmController {
                         departureTime += (24*3600);
                     }
                     
-                    
                     let earliestTargetStopArrival = this.earliestArrivalTime[targetStops[0]];
                     for(let l = 1; l < targetStops.length; l++){
                         if(this.earliestArrivalTime[targetStops[i]] < earliestTargetStopArrival){
@@ -177,8 +141,6 @@ export class RaptorAlgorithmController {
                 }
             }
             
-
-            
             let numberOfMarkedStops = this.markedStops.length;
             for(let i = 0; i < numberOfMarkedStops; i++){
                 let markedStop = this.markedStops[i];
@@ -204,13 +166,6 @@ export class RaptorAlgorithmController {
                     }
                 }
             }
-            
-            // console.log('Part 0: ' + this.timePart0)
-            // console.log('Part 1: ' + this.timePart1)
-            // console.log('Part 2: ' + this.timePart2)
-            // console.log('Part 3: ' + this.timePart3)
-            // console.log('Part 4: ' + this.timePart4)
-            // console.log('Part 5: ' + this.timePart5)
 
             if(this.markedStops.length === 0){
                 break;
@@ -279,7 +234,6 @@ export class RaptorAlgorithmController {
         let earliestArrival = this.earliestArrivalTimePerRound[k-1][pi];
         let earliestArrivalDayOffset = Converter.getDayOffset(earliestArrival);
         
-        
         for(let i = 0; i < stopTimes.length; i++) {
             let stopTime = stopTimes[i];
             if(stopTime.departureTime + earliestArrivalDayOffset > earliestArrival) {
@@ -312,7 +266,7 @@ export class RaptorAlgorithmController {
         return earliestTripInfo;
     }
 
-    public static getJourneyResponse(sourceStops: number[], targetStops: number[]){
+    public static getJourneyResponse(sourceStops: number[], targetStops: number[]): JourneyResponse {
         let earliestTargetStopArrival = this.earliestArrivalTime[targetStops[0]];
         let earliestTargetStopId = targetStops[0];
         for(let i = 1; i < targetStops.length; i++){
@@ -363,6 +317,6 @@ export class RaptorAlgorithmController {
                 }
             }
         }
-        console.log(journeyResponse);
+        return journeyResponse;
     }
 }
