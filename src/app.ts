3e42a0ffa684a9a2b5d7657edd8865b4ef66ccb6
Jurek Sander <44340639+sanderjk5@users.noreply.github.com>
2021-09-26 11:17:23 +0200

    refactor: app and data manipulator

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,6 @@ import routes from './server/routes';
 import cors from 'cors';
 import { Importer } from './data/importer';
 import { Generator } from './data/generator';
-import { RaptorAlgorithmController } from './server/controller/raptorAlgorithmController';
-import { TestController } from './server/controller/testController';
-import { GoogleTransitData } from './data/google-transit-data';
-import { Converter } from './data/converter';
-import { Calculator } from './data/calculator';
 
 const app = express();
 
@@ -19,90 +14,7 @@ Generator.generateValidRoutes();
 Generator.generateSortedConnections();
 // generates footpaths which can be used by raptor and csa
 Generator.generateFootpaths();
-TestController.testAlgorithms();
-
-// let firstStopTimeOfTrip = GoogleTransitData.STOPTIMESOFATRIP[19900];
-// for(let j = firstStopTimeOfTrip; j < GoogleTransitData.STOPTIMES.length; j++) {
-//   let stopTime = GoogleTransitData.STOPTIMES[j];
-//   if(19900 !== stopTime.tripId){
-//     break;
-//   }
-//   console.log(stopTime)
-//   console.log(GoogleTransitData.STOPS[stopTime.stopId])
-// }
-
-// firstStopTimeOfTrip = GoogleTransitData.STOPTIMESOFATRIP[19894];
-// for(let j = firstStopTimeOfTrip; j < GoogleTransitData.STOPTIMES.length; j++) {
-//   let stopTime = GoogleTransitData.STOPTIMES[j];
-//   if(19894 !== stopTime.tripId){
-//     break;
-//   }
-//   console.log(stopTime)
-//   console.log(GoogleTransitData.STOPS[stopTime.stopId])
-// }
-
-// let routeId = GoogleTransitData.TRIPS[10282].routeId
-// console.log(10282)
-// console.log(routeId)
-// for(let i = 0; i < GoogleTransitData.STOPSOFAROUTE[routeId].length; i++){
-//   let stopId = GoogleTransitData.STOPSOFAROUTE[routeId][i];
-//   console.log(GoogleTransitData.STOPS[stopId].name)
-// }
-
-// routeId = GoogleTransitData.TRIPS[8825].routeId
-// console.log(8825)
-// console.log(routeId)
-// for(let i = 0; i < GoogleTransitData.STOPSOFAROUTE[routeId].length; i++){
-//   let stopId = GoogleTransitData.STOPSOFAROUTE[routeId][i];
-//   console.log(GoogleTransitData.STOPS[stopId].name)
-// }
-
-// routeId = GoogleTransitData.TRIPS[23130].routeId
-// console.log(23130)
-// console.log(routeId)
-// // for(let i = 0; i < GoogleTransitData.STOPSOFAROUTE[routeId].length; i++){
-// //   let stopId = GoogleTransitData.STOPSOFAROUTE[routeId][i];
-// //   console.log(GoogleTransitData.STOPS[stopId].name)
-// // }
-
-// console.log(GoogleTransitData.STOPSOFAROUTE[routeId]);
-
-// routeId = GoogleTransitData.TRIPS[23131].routeId
-// console.log(23131)
-// console.log(routeId)
-// for(let i = 0; i < GoogleTransitData.STOPSOFAROUTE[routeId].length; i++){
-//   let stopId = GoogleTransitData.STOPSOFAROUTE[routeId][i];
-//   console.log(GoogleTransitData.STOPS[stopId].name)
-// }
-
-// routeId = GoogleTransitData.TRIPS[18967].routeId
-// console.log(18967)
-// console.log(routeId)
-// for(let i = 0; i < GoogleTransitData.STOPSOFAROUTE[routeId].length; i++){
-//   let stopId = GoogleTransitData.STOPSOFAROUTE[routeId][i];
-//   console.log(GoogleTransitData.STOPS[stopId].name)
-// }
-
-// routeId = GoogleTransitData.TRIPS[30707].routeId
-// console.log(30707)
-// console.log(routeId)
-// for(let i = 0; i < GoogleTransitData.STOPSOFAROUTE[routeId].length; i++){
-//   let stopId = GoogleTransitData.STOPSOFAROUTE[routeId][i];
-//   console.log(GoogleTransitData.STOPS[stopId].name)
-// }
-
-// routeId = GoogleTransitData.TRIPS[21426].routeId
-// console.log(21426)
-// console.log(routeId)
-// for(let i = 0; i < GoogleTransitData.STOPSOFAROUTE[routeId].length; i++){
-//   let stopId = GoogleTransitData.STOPSOFAROUTE[routeId][i];
-//   console.log(GoogleTransitData.STOPS[stopId].name)
-// }
-
-// for(let i = -18; i < 18; i++){
-//   console.log(Calculator.moduloSeven(i))
-// }
-
+//TestController.testAlgorithms();
 
 const port = 1337;
 const corsOptions = {
--- src/data/calculator.ts
+++ src/data/calculator.ts
@@ -1,5 +1,27 @@
 export class Calculator {
+
+    /**
+     * Calculates correct modulo seven result (even for negative numbers).
+     * @param value 
+     * @returns 
+     */
     public static moduloSeven(value: number): number{
         return (value % 7 + 7) % 7;
+    }
+
+     /**
+     * Calculates the approximated distance between two coordinates.
+     * @param lat1 
+     * @param lat2 
+     * @param lon1 
+     * @param lon2 
+     * @returns 
+     */
+      public static calculateDistance(lat1: number, lat2: number, lon1: number, lon2: number): number {
+        const R = 111.319;
+        const x = (lon2 - lon1) * Math.cos(0.00872664626*(lat2+lat1));
+        const y = lat2 - lat1;
+        const d = R * Math.sqrt(x*x + y*y);
+        return d;
     }
 }
