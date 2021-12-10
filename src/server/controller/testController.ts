ccc626780fc6ea8f59d9a9e06ff5b67ea81d07d1
Jurek Sander <44340639+sanderjk5@users.noreply.github.com>
2021-12-10 11:02:19 +0100

    feat: updated alpha value

--- src/server/controller/testController.ts
+++ src/server/controller/testController.ts
@@ -699,7 +699,7 @@ export class TestController {
 
         DelayTestController.addDelaysToTrips();
 
-        let alphas = [2];
+        let alphas = [3];
 
         let numberOfFilesPerAlpha = 20;
         let numberOfRequestsPerFile = 50;
